ba5230ae6e2f8a3b41501cf7381b9de52d565fa9
Meiryanne Martins <meirymartinsp.rodrigues@gmail.com>
2022-10-23 13:57:54 -0300

    Create component main

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,34 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
+    node: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
+    'plugin:react-hooks/recommended',
   ],
-
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/state-in-constructor': 0
   },
 };
